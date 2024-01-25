457ba2a673dc75cfccbb9b039de1ddafbebef3da
AlexAxello <A.x.e.l.l.o@yandex.ru>
2024-01-25 19:15:16 +0300

    player movement fixes

--- Assets/Character/CharacterController.cs
+++ Assets/Character/CharacterController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
@@ -7,20 +6,25 @@ public class CharacterController : MonoBehaviour
     
     [Header("Characteristics")] 
     public float speed = 3;
-    public float runMultiplier;
+    public float jumpHeight = 10;
+    public float speedMultiplier;
 
     [Header("Physic Model")] 
     [SerializeField] private Rigidbody rb;
+    private float distToGround;
 
-    public GameObject cameraAnchor;
-    private new Camera camera;
-    private Plane plane;
-    private Vector3 viewDirection;
-    private float scrollSensetivity = 3;
+    [SerializeField] private Transform cameraAnchorH;
+    [SerializeField] private Transform cameraAnchorV;
+    [SerializeField] private Transform cameraTransform;
+    public Transform characterAnchor;
+    
+    [SerializeField] private float scrollSensetivity = 3;
 
     [Header("Move State")] 
     [SerializeField] private bool isMove;
     [SerializeField] private bool isRun;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isStandby;
     
     private void Start()
     {
@@ -28,81 +32,110 @@ public class CharacterController : MonoBehaviour
         Cursor.visible = false;
         
         character = new Character();
-        
-        camera = Camera.main;
-        plane = new Plane(Vector3.up, Vector3.zero);
+
+        cameraTransform = Camera.main!.transform;
+        cameraAnchorV = cameraTransform.transform.parent;
+        cameraAnchorH = cameraAnchorV.transform.parent;
         
         rb = GetComponent<Rigidbody>();
+        
+        isGrounded = true;
+        
         Debug.Log($"level: {character.health.statLevel.level}, exp: {character.health.statLevel.exp}, exp to next level: {character.health.statLevel.expToNextLevel}");
         Debug.Log($"HP: {character.health.statValue.currentValue}");
     }
-    
+
     private void Update()
     {
+        CameraRotate();
         Move();
-        //LookAtMousePosition();
-
     }
-    
-    private void Move()
+
+    private void CameraRotate()
     {
-        var moveInputForward = Input.GetAxisRaw("Vertical") * rb.transform.forward;
-        var moveInputSide = Input.GetAxisRaw("Horizontal") * rb.transform.right;
         var angularInputVertical = Input.GetAxisRaw("Mouse Y");
         var angularInputHorizontal = Input.GetAxisRaw("Mouse X");
         var scrollInput = Input.GetAxisRaw("Mouse ScrollWheel");
-        
+
         //Camera movement
-        transform.Rotate(0, angularInputHorizontal, 0);
-        cameraAnchor.transform.Rotate(-angularInputVertical, 0, 0);
+        cameraAnchorH.Rotate(0, angularInputHorizontal, 0);
+        cameraAnchorV.Rotate(-angularInputVertical, 0, 0);
 
-        var eulerAngles = cameraAnchor.transform.localEulerAngles;
-        
-        if (eulerAngles.y != 0 || eulerAngles.z != 0)
+        if (cameraAnchorV.localEulerAngles.y != 0)
         {
-            cameraAnchor.transform.Rotate(angularInputVertical, 0, 0);
+            cameraAnchorV.Rotate(angularInputVertical, 0, 0);
         }
-        
-        camera.transform.Translate(0,0, scrollInput * scrollSensetivity, Space.Self);
 
-        if (camera.transform.localPosition.z is > -1 or < -8)
+        cameraTransform.Translate(0, 0, scrollInput * scrollSensetivity, Space.Self);
+
+        if (cameraTransform.localPosition.z is > -1 or < -8)
         {
-            camera.transform.Translate(0, 0, -scrollInput*scrollSensetivity, Space.Self);
+            cameraTransform.transform.Translate(0, 0, -scrollInput * scrollSensetivity, Space.Self);
         }
+    }
+
+    private void Move()
+    {
+        var moveInputForward = Input.GetAxisRaw("Vertical");
+        var moveInputSide = Input.GetAxisRaw("Horizontal");
         
-        //Character Movement
-        if (Input.GetKey(KeyCode.LeftShift))
+        
+        //MoveState Check
+        if (Input.GetKey(KeyCode.LeftShift) && !isStandby)
         {
-            runMultiplier = 1.5f;
+            speedMultiplier = 1.5f;
             isRun = true;
         }
         else
         {
-            runMultiplier = 1;
+            speedMultiplier = 1;
             isRun = false;
         }
 
-        var velocity = Vector3.Normalize(moveInputForward + moveInputSide);
-        rb.velocity = speed * runMultiplier * velocity;
-    }
-
-    private void LookAtMousePosition()
-    {
-        var position = transform.position;
-        if (Input.GetMouseButton(1) && !isRun)
+        if (Input.GetMouseButton(1))
         {
-            var mousepointRay = camera.ScreenPointToRay(Input.mousePosition);
-            plane.Raycast(mousepointRay, out var distance);
-            var point = mousepointRay.GetPoint(distance);
-            
-            var rotation = Mathf.Atan2(point.x - position.x, point.z - position.z) * Mathf.Rad2Deg;
-            LerpRotation(rotation);
+            isStandby = true;
+            speedMultiplier = 0.5f;
+        }
+        else
+        {
+            isStandby = false;
+        }
+        //Character Movement
+        var velocity = Vector3.Normalize(cameraAnchorH.forward * moveInputForward + cameraAnchorH.right * moveInputSide);
+        rb.velocity = speed * speedMultiplier * velocity + Vector3.up * rb.velocity.y;
+        
+        if (isStandby)
+        {
+            LerpRotateTo(cameraAnchorH.forward);
+        }
+        else
+        {
+            if (velocity.x != 0 && velocity.z != 0)
+            {
+                LerpRotateTo(velocity);
+            }
+        }
+        
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
         }
     }
 
-    private void LerpRotation(float targetAngle)
+    private void OnTriggerEnter(Collider other)
     {
-        var rot = Quaternion.Euler(0, targetAngle, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rot, 0.1f);
+        isGrounded = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
+    }
+
+    private void LerpRotateTo(Vector3 direction)
+    {
+        var target = Quaternion.LookRotation(direction);
+        characterAnchor.rotation = Quaternion.RotateTowards(characterAnchor.rotation, target, 360 * Time.deltaTime);
     }
 }
