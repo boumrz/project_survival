8451652a4d3efbea84c5c9e82a19b1f7ffefb2f5
AlexAxello <A.x.e.l.l.o@yandex.ru>
2024-02-04 19:04:28 +0300

    minor changes

--- Assets/Scripts/Inventory/QuickslotInventory.cs
+++ Assets/Scripts/Inventory/QuickslotInventory.cs
@@ -1,8 +1,4 @@
-﻿using JetBrains.Annotations;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class QuickslotInventory : MonoBehaviour
