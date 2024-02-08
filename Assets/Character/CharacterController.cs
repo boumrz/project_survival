00eb77553f911808a91b93267797a74b80c87906
AlexAxello <A.x.e.l.l.o@yandex.ru>
2024-02-08 21:15:44 +0300

    animations

--- Assets/Character/CharacterController.cs
+++ Assets/Character/CharacterController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,7 +6,9 @@ public class CharacterController : MonoBehaviour
 {
     public Character character;
 
-    [Header("Characteristics")] public float speed = 3;
+    [Header("Characteristics")] 
+    
+    public float speed = 2;
     public float jumpHeight = 10;
     public float speedMultiplier;
 
@@ -16,8 +19,9 @@ public class CharacterController : MonoBehaviour
 
     public Image staminaBarFill;
 
-    [Header("Physic Model")] [SerializeField]
-    private Rigidbody rb;
+    [Header("Physic Model")] 
+    
+    [SerializeField] private Rigidbody rb;
 
     private float distToGround;
     [SerializeField] private float cameraTargetPosition;
@@ -29,13 +33,24 @@ public class CharacterController : MonoBehaviour
 
     private float lastStaminaSpendTime;
 
-    [Header("Move State")] [SerializeField]
-    private bool isMove;
+    [Header("Move State")]
 
+    [SerializeField] private bool isMove;
     [SerializeField] private bool isRun;
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool isStandby;
 
+    [Header("Animation")] 
+    
+    public Animator characterAnimator;
+
+    private static readonly int move = Animator.StringToHash("isMove");
+    private static readonly int run = Animator.StringToHash("isRun");
+    private static readonly int standby = Animator.StringToHash("isStandby");
+    private static readonly int direction = Animator.StringToHash("direction");
+    
+    private int standbyLayerIndex;
+    
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -55,6 +70,8 @@ public class CharacterController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         isGrounded = true;
+        
+        standbyLayerIndex = characterAnimator.GetLayerIndex("Standby Layer");
 
         // Debug.Log($"level: {character.health.statLevel.level}, exp: {character.health.statLevel.exp}, exp to next level: {character.health.statLevel.expToNextLevel}");
         // Debug.Log($"HP: {character.health.statValue.currentValue}");
@@ -110,27 +127,73 @@ public class CharacterController : MonoBehaviour
         var moveInputSide = Input.GetAxisRaw("Horizontal");
 
         //MoveState Check
-        isMove = (moveInputForward != 0 || moveInputSide != 0);
-
-        if (Input.GetKey(KeyCode.LeftShift) && !isStandby)
+        
+        switch (moveInputForward)
         {
-            speedMultiplier = 1.5f;
+            case > 0:
+            {
+                characterAnimator.SetInteger(direction, 0);
+                break;
+            }
+            case < 0:
+            {
+                characterAnimator.SetInteger(direction, 2);
+                break;
+            }
+            default:
+            {
+                switch (moveInputSide)
+                {
+                    case > 0:
+                    {
+                        characterAnimator.SetInteger(direction, 1);
+                        break;
+                    }
+                    case < 0:
+                    {
+                        characterAnimator.SetInteger(direction, 3);
+                        break;
+                    }
+                    default:
+                    {
+                        characterAnimator.SetInteger(direction, 4);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        
+        isMove = (moveInputForward != 0 || moveInputSide != 0);
+        characterAnimator.SetBool(move, isMove);
+
+        if (Input.GetKey(KeyCode.LeftShift) && !isStandby && stamina.statValue.currentValue > 0)
+        {
+            speedMultiplier = 2f;
             isRun = true;
+            characterAnimator.SetBool(run, isRun);
         }
         else
         {
             speedMultiplier = 1;
             isRun = false;
+            characterAnimator.SetBool(run, isRun);
         }
 
         if (Input.GetMouseButton(1))
         {
             isStandby = true;
             speedMultiplier = 0.5f;
+            var weight = Mathf.MoveTowards(characterAnimator.GetLayerWeight(standbyLayerIndex), 1, 5 * Time.deltaTime);
+            characterAnimator.SetLayerWeight(standbyLayerIndex, weight);
+            characterAnimator.SetBool(standby, isStandby);
         }
         else
         {
             isStandby = false;
+            var weight = Mathf.MoveTowards(characterAnimator.GetLayerWeight(standbyLayerIndex), 0, 5 * Time.deltaTime);
+            characterAnimator.SetLayerWeight(standbyLayerIndex, weight);
+            characterAnimator.SetBool(standby, isStandby);
         }
 
         //Character Movement
@@ -174,6 +237,7 @@ public class CharacterController : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         isGrounded = false;
+        
     }
 
     private void LerpRotateTo(Vector3 direction)
